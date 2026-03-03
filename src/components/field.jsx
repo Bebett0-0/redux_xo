f845b07a0320ea1a052d906b663eb1529f9174d7
MalyshevNikita <whyteincsos@gmail.com>
2026-03-03 17:04:05 +0400

    seecond

--- src/components/field.jsx
+++ src/components/field.jsx
@@ -4,7 +4,7 @@ export const Field = ({ value, onClick }) => {
   // компонент Field, который представляет собой отдельную клетку игрового поля, принимает два пропса: value (значение клетки, может быть "X", "O" или null) и onClick (функция, которая будет вызываться при клике на эту клетку)
   return (
     <button className={styles.field} onClick={onClick}>
-      {value === "X" ? "X" : value === "O" ? "O" : ""}
+      {value === "X" ? "X" : value === "O" ? "O`" : ""}
     </button>
   );
 };
